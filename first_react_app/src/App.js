89e323ce491c34733581ce5d33412ba6d02ccd51
enoch-strok <enochstrok@outlook.com>
2021-01-05 17:51:44 -0800

    completed app

--- first_react_app/src/App.js
+++ first_react_app/src/App.js
@@ -4,23 +4,35 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>Hello Dojo!!</p>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      <h1>Hello Dojo!!</h1>
+      <ul>
+        <h3>Things I need to do:</h3>
+      </ul>
+      <li>Learn React</li>
+      <li>Climb Mt. Everest</li>
+      <li>Run a marathon</li>
+      <li>Feed the dogs</li>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+{/* <header className="App-header">
+  <img src={logo} className="App-logo" alt="logo" />
+  <p>
+    Edit <code>src/App.js</code> and save to reload.
+  </p>
+  <a
+    className="App-link"
+    href="https://reactjs.org"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Learn React
+  </a>
+</header> */}
